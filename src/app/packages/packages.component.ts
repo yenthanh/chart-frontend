3af6e8d0c82b90db027053e248706121a23d3f44
Michael Pham <michael@sapotacorp.vn>
2023-06-27 13:44:18 +0700

    integrate call api

--- src/app/packages/packages.component.ts
+++ src/app/packages/packages.component.ts
@@ -1,21 +1,29 @@
 ﻿import { Component } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import { AccountService } from '@app/_services';
-
+import { PackagesService } from './packages.service';
 @Component({ templateUrl: 'packages.component.html' })
 export class PackagesComponent {
-    account = this.accountService.accountValue;
-    Highcharts: typeof Highcharts = Highcharts; // required
-    chartConstructor: string = 'chart'; // optional string, defaults to 'chart'
-    chartOptions: Highcharts.Options = {
-        series: [{
-          data: [1, 2, 3],
-          type: 'line'
-        }]
-      }; // required
-    chartCallback: Highcharts.ChartCallbackFunction = function (chart) { } // optional function, defaults to null
-    updateFlag: boolean = false; // optional boolean
-    oneToOneFlag: boolean = true; // optional boolean, defaults to false
-    runOutsideAngular: boolean = false; 
-    constructor(private accountService: AccountService) { }
+  // items;
+  account = this.accountService.accountValue;
+  Highcharts: typeof Highcharts = Highcharts; // required
+  chartConstructor: string = 'chart'; // optional string, defaults to 'chart'
+  chartOptions: Highcharts.Options = {
+    series: [{
+      data: [1, 2, 3],
+      type: 'line',
+    }]
+  }; // required
+  chartCallback: Highcharts.ChartCallbackFunction = function (chart) { } // optional function, defaults to null
+  updateFlag: boolean = false; // optional boolean
+  oneToOneFlag: boolean = true; // optional boolean, defaults to false
+  runOutsideAngular: boolean = false;
+  constructor(private accountService: AccountService, private packagesService: PackagesService) { }
+
+  fetchItems() {
+    this.packagesService.getItems().subscribe((response) => {
+      console.log(response);
+      // this.items = response;
+    });
+  }
 }
---
+++ src/app/packages/packages.service.ts
@@ -0,0 +1,13 @@
+﻿import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class PackagesService {
+    constructor(private http: HttpClient) { }
+    getItems() {
+        const apiUrl = 'http://127.0.0.1:8000/api/package-warehouses/chart?time=day&warehouses[]=FRANCE&warehouses[]=PORTUGAL&date_end=2023-06-27&action_ids[]=3&action_ids[]=5&action_ids[]=10&action_ids[]=31&date_start=2023-06-01'; // Replace with your API endpoint URL
+        return this.http.get(apiUrl);
+    }
+}
